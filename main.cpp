eaf024813bcd02f84110d43dab83538be6f1259f
Harman Kalair <harman.kalair@ada.ac.uk>
2021-03-18 12:06:13 +0000

    added menu options

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
 
+//Main menu function to print single or multiplayer and returns the user choice
 int main() {
-  std::cout << "Hello World!\n";
+  cout << "▄▀█ █▀▄ ▄▀█ █▀ █░█ █ █▀█" << endl;
+  cout << "█▀█ █▄▀ █▀█ ▄█ █▀█ █ █▀▀" << endl;
+  
+
+
+
+
+	cout << "*********************\n";
+	int choice;
+	cout << "1. One player v computer game\n2. Two player game\n3. One player v computer (salvo) game.\n4. Two player game (salvo) game\n5. One player v computer (hidden mines) game\n6. Two player game (hidden mines) game\n7. Computer v computer (hidden mines)\n8. Quit\n";
+	cout << "*********************\n";
+	cin >> choice;
+	return choice;
 }
